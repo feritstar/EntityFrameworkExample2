4643840fd0c5ca1c04341ab845beb068678e4b07
FeritYILDIZ <feritstar@gmail.com>
2023-06-24 22:13:04 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EntityFrameworkExample2.DataModel;
+
+namespace EntityFrameworkExample2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DbSinavOgrenciEntities db = new DbSinavOgrenciEntities();
+
+        private void buttonOrderByAscThenGroupBy_Click(object sender, EventArgs e)
+        {
+            var query = db.TBLOGRENCI.
+                OrderBy(x => x.Sehir).
+                    GroupBy(y => y.Sehir).
+                        Select(z =>
+                            new
+                            {
+                                City = z.Key,
+                                Sum = z.Count()
+                            });
+            dataGridView1.DataSource = query.ToList();
+        }
+
+        private void buttonOrderByDescThenGroupBy_Click(object sender, EventArgs e)
+        {
+            var query = db.TBLOGRENCI.
+                            OrderBy(x => x.Sehir).
+                                GroupBy(y => y.Sehir).
+                                    Select(z =>
+                                        new
+                                        {
+                                            City = z.Key,
+                                            Sum = z.Count()
+                                        }).
+                                            OrderByDescending(z=>z.City);
+            dataGridView1.DataSource = query.ToList();
+        }
+    }
+}
